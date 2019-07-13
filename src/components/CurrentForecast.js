e87e0f9151f3632aa95fb11515d1899900ba1676
VBTX <jv.svedaite@gmail.com>
2019-07-13 13:49:30 -0500

    add moment library and current forecast object

--- src/components/CurrentForecast.js
+++ src/components/CurrentForecast.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment'
 
 
 const CurrentForecast = ({forecast: {apparentTemperature, humidity, precipProbability, summary, temperature, time}}) =>
 <div>
-<h2>Current Forecast</h2>
-<div style = {{ border: 'solid 1px black', padding: '16px', margin: '35px'}}>
+<h2>Current Forecast for {moment().format('LL')}</h2>
+<div>
 <h3>{summary}</h3>
-	<p>Current Time: {time}</p>
-	<p>Temperature: {temperature}</p>
-	<p>Feels Like: {apparentTemperature}</p>
+	<p>Temperature: {Math.round(temperature)}</p>
+	<p>Feels Like: {Math.round(apparentTemperature)}</p>
 	<p>Chance of Percipitation: {precipProbability}</p>
 	<p>Humidity: {humidity}</p>
 </div>
