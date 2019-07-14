506634f7a3262cc1a5cbbd87c476b30c93139d81
VBTX <jv.svedaite@gmail.com>
2019-07-14 06:31:06 -0500

    add provider

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,19 @@
 
 import {createStore} from 'redux'
 import thunk from 'redux-thunk'
+import fetchingData from '../reducers/fetchingDataReducer';
+import weatherData from '../reducers/weatherDataReducer';
+import route from '../reducers/routeReducer';
 
 
 const middlewares = [thunk]
+
+
+export default createStore(
+	fetchingData,
+	weatherData,
+	route,
+	applyMiddleware(...middlewares))
 
   // store = {fetchingData: true,
   //   //once the fetching of data will be finished, set it to false
