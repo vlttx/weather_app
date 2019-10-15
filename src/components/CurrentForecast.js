b725ba3197b67905ea1df6d0680b0ba955fa072f
VBTX <jv.svedaite@gmail.com>
2019-10-14 21:20:31 -0500

    separate css from js

--- src/components/CurrentForecast.js
+++ src/components/CurrentForecast.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import moment from 'moment'
+import moment from 'moment';
+import './CurrentForecast.css';
 
-
-const CurrentForecast = ({forecast: {apparentTemperature, humidity, precipProbability, summary, temperature, time}}) =>{
-return (<div>
-<br/>
-<h2>Current Forecast for {moment().format('LL')}</h2>
-<div style={{border: 'solid 2px black', padding: '16px', margin: '16px', width:'200px', display:'inline-block'}}>
-<h4>{summary}</h4>
-	<p>Temperature: {Math.round(temperature)}</p>
-	<p>Feels Like: {Math.round(apparentTemperature)}</p>
-	<p>Chance of Percipitation: {precipProbability}</p>
-	<p>Humidity: {humidity}</p>
-
-
-</div>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-</div>)
-}
+const CurrentForecast = ({
+	forecast: {
+		apparentTemperature,
+		humidity,
+		precipProbability,
+		summary,
+		temperature,
+		time
+	}
+}) => {
+	return (
+		<div>
+			<br />
+			<h2>Current Forecast for {moment().format('LL')}</h2>
+			<div className='current'>
+				<h4>{summary}</h4>
+				<p>Temperature: {Math.round(temperature)}</p>
+				<p>Feels Like: {Math.round(apparentTemperature)}</p>
+				<p>Chance of Percipitation: {precipProbability}</p>
+				<p>Humidity: {humidity}</p>
+			</div>
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+		</div>
+	);
+};
 export default CurrentForecast;
