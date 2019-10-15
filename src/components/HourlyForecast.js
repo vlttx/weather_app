84a983bfe41d565b089c32f302ca8d985d9a8236
VBTX <jv.svedaite@gmail.com>
2019-10-14 21:25:31 -0500

    separate css from js

--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import moment from 'moment'
+import moment from 'moment';
+import './HourlyForecast.css';
 
-
-const HourlyForecast = ({forecast}) => {
-	const renderForecasts = forecast.map(({apparentTemperature, humidity, precipProbability, summary, temperature, time} , index) =>
-		
-		<div key={index} style={{border: 'solid 2px blue', padding: '16px', margin: '16px', width:'200px', display:'inline-block'}}>
-		<h3>{moment.unix(time).format('LT')}</h3>
-		<p>Temperature: {Math.round(temperature)}</p>
-		<p>Feels Like: {Math.round(apparentTemperature)}</p>
-		<p>Chance of Percipitation: {precipProbability}</p>
-		<p>Humidity: {humidity}</p>
+const HourlyForecast = ({ forecast }) => {
+	const renderForecasts = forecast.map(
+		(
+			{
+				apparentTemperature,
+				humidity,
+				precipProbability,
+				summary,
+				temperature,
+				time
+			},
+			index
+		) => (
+			<div key={index} className='hourly'>
+				<h3>{moment.unix(time).format('LT')}</h3>
+				<p>Temperature: {Math.round(temperature)}</p>
+				<p>Feels Like: {Math.round(apparentTemperature)}</p>
+				<p>Chance of Percipitation: {precipProbability}</p>
+				<p>Humidity: {humidity}</p>
+			</div>
+		)
+	);
+	return (
+		<div>
+			<br />
+			<h2>Hourly Forecast</h2>
+			{renderForecasts}
 		</div>
-)
- return (
- 	<div><br/><h2>Hourly Forecast</h2>
- 	{renderForecasts}
- 	</div>)
-}
+	);
+};
 export default HourlyForecast;
